46ab19707c6427847c9735350391e62be97c3444
Eduardo <eoglc15@gmail.com>
2024-11-15 17:28:08 -0500

    código para enviar Email
    Se agregaron las líneas de código para el envío de emails. Y un excel con los emails destinos

--- sendGmail.py
+++ sendGmail.py
@@ -2,9 +2,9 @@ import smtplib
 import pandas as pd
 
 # Set credentials - establecer variables para la conexión al gmail
-name_account = "Sandreke"
-email_account = "sandreke99@gmail.com"
-password_account = "zmpwpbqxfdnoxpha" # password created in step 1.3.
+name_account = "One Byte Ventures"
+email_account = "onebyteventures@gmail.com"
+password_account = "fhhnibcadtpnarlw" # password created in step 1.3.
 
 # 'smtp.gmail.com' and 465 port refer to Gmail as provider
 # Change these arguments if you are using another one
@@ -15,10 +15,41 @@ server.login(email_account, password_account)
 
 # Read the file that contains at least names & email addresses
 # Subjects & messages can be personalized, but we use them as input
-email_df = pd.read_excel("Data/Emails.xlsx")
+email_df = pd.read_excel("Data/emails.xlsx")
 
 # Get all names, email addresses, subjects & messages
 all_names = email_df['Name']
 all_emails = email_df['Email']
 all_subjects = email_df['Subject']
 all_messages = email_df['Message']
+
+
+# 
+for i in range(len(email_df)):
+
+    email = all_emails[i]
+
+    # Personalized subject
+    subject = all_subjects[i] + ', ' + all_names[i] + '!'
+
+    # Personalized message
+    message = ('Hey, ' + all_names[i] + '!\n\n' +
+              all_messages[i] + '\n\n'
+              'Te deseamos lo mejor,\n' +
+              name_account)
+
+    # Generate email to be sent
+    sent_email = ("From: {0} <{1}>\n"
+                  "To: {2} <{3}>\n"
+                  "Subject: {4}\n\n"
+                  "{5}"
+                  .format(name_account, email_account, name, email, subject, message))
+
+    # Send emails, the reason will be displayed in case you get an error
+    try:
+        server.sendmail(email_account, [email], sent_email) # [email] is a list that can contain multiple emails
+    except Exception:
+        print('Could not send email to {}. Error: {}\n'.format(email, str(Exception)))
+
+# Close smtp server
+server.close()
