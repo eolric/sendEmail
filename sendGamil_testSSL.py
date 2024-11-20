9de4ff0a6c1741ef1883544129e46de23e4ddf6a
Eduardo <eoglc15@gmail.com>
2024-11-20 12:27:46 -0500

    Se arregló el problema de las imágenes que no se enviaban en el cuerpo del correo

--- sendGamil_testSSL.py
+++ sendGamil_testSSL.py
@@ -4,7 +4,7 @@ from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 import pandas as pd
 from jinja2 import Template
-import base64
+# import base64
 
 # Configuración del servidor SMTP
 smtp_server = "smtp.gmail.com"
@@ -18,31 +18,44 @@ email_df = pd.read_excel("files/emails.xlsx")
 destinatarios = email_df['Email']
 
 # Contenido del correo
-asunto = "Asunto YOY, qué asunto ponemos?"
+asunto = "Asunto YOY, invierte"
 with open("files/email.html", "r", encoding='utf-8') as file:
     template = Template(file.read())
 
-with open("files/foot_image.png", "rb") as imagen:
-        logoFootpy = base64.b64encode(imagen.read()).decode()
+# with open("files/foot_image.png", "rb") as imagen:
+#         logoFootpy = base64.b64encode(imagen.read()).decode()
 
-cuerpo = template.render(
-        logoFoot  = logoFootpy
-        )
+# Renderizar el cuerpo HTML
+cuerpo_html = template.render()
+
+# cuerpo = template.render(
+#         logoFoot  = logoFootpy
+#         )
 
 
 # Función para enviar el correo
-def enviar_correo(destinatario):
-    mensaje = MIMEMultipart()
+def enviar_correo(destinatario, cuerpo_html):
+    mensaje = MIMEMultipart('related')
     mensaje['From'] = smtp_user
     mensaje['To'] = destinatario
     mensaje['Subject'] = asunto
     
     # mensaje.attach(MIMEText(cuerpo, 'plain'))
-    mensaje.attach(MIMEText(cuerpo, 'html')) # Especificamos el tipo de texto en HTML
+    mensaje.attach(MIMEText(cuerpo_html, 'html')) # Especificamos el tipo de texto en HTML
     
-    # Agregamos la firma como documento interno (no adjunto)
+    # Agregar imágenes con Content-ID 
+    imagenes = [ 
+        ("files/head_image.png", "head_YOY"), 
+        ("files/iconWapp.png", "iWapp"), 
+        ("files/foot_image.png", "firma_Innverto")
+    ]
     
-        
+    for img_path, cid in imagenes: 
+        with open(img_path, "rb") as img_file: 
+            img = MIMEImage(img_file.read()) 
+            img.add_header("Content-ID", f"<{cid}>") 
+            img.add_header("Content-Disposition", "inline", filename=img_path.split('/')[-1]) 
+            mensaje.attach(img)
 
     try:
         servidor = smtplib.SMTP_SSL(smtp_server, smtp_port)
@@ -55,4 +68,4 @@ def enviar_correo(destinatario):
 
 # Enviar correos a todos los destinatarios
 for destinatario in destinatarios:
-    enviar_correo(destinatario)
+    enviar_correo(destinatario, cuerpo_html)
