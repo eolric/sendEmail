8b2854d90072530769e21e7ef92677e0b6c9d81a
Eduardo <eoglc15@gmail.com>
2024-11-24 21:21:02 -0500

    actualización de SendGmail_testSSL como librería y script para ejecutar

--- sendGamil_testSSL.py
+++ sendGamil_testSSL.py
@@ -4,68 +4,74 @@ from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 import pandas as pd
 from jinja2 import Template
-# import base64
 
-# Configuración del servidor SMTP
-smtp_server = "smtp.gmail.com"
-smtp_port = 465  # Puerto para SSL
-smtp_user = "onebyteventures@gmail.com"
-smtp_password = "fhhn ibca dtpn arlw"
+class sendEmail:
+    '''
+    Clase para enviar emails de dataBase.xlsx
+    '''
+    def __init__(self):
+        pass
 
-# Lista de Excel
-# Leer la base de datos
-email_df = pd.read_excel("files/emails.xlsx")
-destinatarios = email_df['Email']
+    def sendEmail_func():
+        '''
+        Función para enviar emails a través de Gmail\n
+        Se utiliza el puerto 465, para hacer el envío por SSL\n
+        Se debe de tener la contraseña para aplicaciones externas
+        '''
+        # Configuración del servidor SMTP
+        smtp_server = "smtp.gmail.com"
+        smtp_port = 465  # Puerto para SSL
+        smtp_user = "onebyteventures@gmail.com"
+        smtp_password = "fhhn ibca dtpn arlw"
 
-# Contenido del correo
-asunto = "Asunto YOY, invierte"
-with open("files/email.html", "r", encoding='utf-8') as file:
-    template = Template(file.read())
+        # Lista de Excel
+        # Leer la base de datos
+        email_df = pd.read_excel("files/dataBase.xlsx")
+        destinatarios = email_df['Email']
 
-# with open("files/foot_image.png", "rb") as imagen:
-#         logoFootpy = base64.b64encode(imagen.read()).decode()
+        # Contenido del correo
+        asunto = "LOTES en Jamundí desde 52 MILLONES"
+        with open("files/email.html", "r", encoding='utf-8') as file:
+            template = Template(file.read())
 
-# Renderizar el cuerpo HTML
-cuerpo_html = template.render()
+        # Renderizar el cuerpo HTML
+        cuerpo_html = template.render()
 
-# cuerpo = template.render(
-#         logoFoot  = logoFootpy
-#         )
+        # Función para enviar el correo
+        def enviar_correo(destinatario, cuerpo_html):
+            '''
 
+            '''
+            mensaje = MIMEMultipart('related')
+            mensaje['From'] = smtp_user
+            mensaje['To'] = destinatario
+            mensaje['Subject'] = asunto
+            
+            mensaje.attach(MIMEText(cuerpo_html, 'html')) # Especificamos el tipo de texto en HTML
+            
+            # Agregar imágenes con Content-ID 
+            imagenes = [ 
+                ("files/head_image.png", "head_YOY"), 
+                ("files/iconWapp.png", "iWapp"), 
+                ("files/foot_image.png", "firma_Innverto")
+            ]
+            
+            for img_path, cid in imagenes: 
+                with open(img_path, "rb") as img_file: 
+                    img = MIMEImage(img_file.read()) 
+                    img.add_header("Content-ID", f"<{cid}>") 
+                    img.add_header("Content-Disposition", "inline", filename=img_path.split('/')[-1]) 
+                    mensaje.attach(img)
 
-# Función para enviar el correo
-def enviar_correo(destinatario, cuerpo_html):
-    mensaje = MIMEMultipart('related')
-    mensaje['From'] = smtp_user
-    mensaje['To'] = destinatario
-    mensaje['Subject'] = asunto
-    
-    # mensaje.attach(MIMEText(cuerpo, 'plain'))
-    mensaje.attach(MIMEText(cuerpo_html, 'html')) # Especificamos el tipo de texto en HTML
-    
-    # Agregar imágenes con Content-ID 
-    imagenes = [ 
-        ("files/head_image.png", "head_YOY"), 
-        ("files/iconWapp.png", "iWapp"), 
-        ("files/foot_image.png", "firma_Innverto")
-    ]
-    
-    for img_path, cid in imagenes: 
-        with open(img_path, "rb") as img_file: 
-            img = MIMEImage(img_file.read()) 
-            img.add_header("Content-ID", f"<{cid}>") 
-            img.add_header("Content-Disposition", "inline", filename=img_path.split('/')[-1]) 
-            mensaje.attach(img)
+            try:
+                servidor = smtplib.SMTP_SSL(smtp_server, smtp_port)
+                servidor.login(smtp_user, smtp_password)
+                servidor.sendmail(smtp_user, destinatario, mensaje.as_string())
+                servidor.quit()
+                print(f"Correo enviado a {destinatario}")
+            except Exception as e:
+                print(f"Error al enviar el correo a {destinatario}: {e}")
 
-    try:
-        servidor = smtplib.SMTP_SSL(smtp_server, smtp_port)
-        servidor.login(smtp_user, smtp_password)
-        servidor.sendmail(smtp_user, destinatario, mensaje.as_string())
-        servidor.quit()
-        print(f"Correo enviado a {destinatario}")
-    except Exception as e:
-        print(f"Error al enviar el correo a {destinatario}: {e}")
-
-# Enviar correos a todos los destinatarios
-for destinatario in destinatarios:
-    enviar_correo(destinatario, cuerpo_html)
+        # Enviar correos a todos los destinatarios
+        for destinatario in destinatarios:
+            enviar_correo(destinatario, cuerpo_html)
